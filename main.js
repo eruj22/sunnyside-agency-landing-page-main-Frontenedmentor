303232301f807027b2aedc889d3880acc50ebb53
eruj22 <prci.prnaver2@gmail.com>
2021-06-19 15:36:57 +0200

    minor styling changes

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const nav = document.querySelector('.nav');
 toggleBtn.addEventListener('click', () => {
     nav.classList.toggle('nav-open');
 });
+
+$(function() {
+    $('.arrow').on('click', function(e) {
+        e.preventDefault();
+        console.log('ss');
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+});
